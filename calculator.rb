0ac6e509c3cff3d0c2d617a44e6cc0359a390754
Jordan Hayes <nosafoundation@gmail.com>
2019-09-11 02:29:06 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,6 @@
 first_number = 2 
 second_number = 1 
 sum = first_number + second_number
-difference = first_
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number \ second_number
